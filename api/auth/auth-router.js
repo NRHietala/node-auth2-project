55ccc8d7f02fd2b9e852de3e3c8eee31c36e0a16
Nathan Hietala <Nrhietala@gmail.com>
2021-02-23 15:23:05 -0600

    Working on auth router

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,14 +1,24 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const router = express().Router();
 
-router.post("/register", (req, res, next) => {
+router.post("/register", async (req, res, next) => {
+  const credentials = req.body;
   try {
+    if (credentials) {
+      // add in validation
+      const rounds = process.env.BCRYPT_ROUNDS || 8;
+      const hashed = bcrypt.hashSync(credentials.password, rounds);
+      credentials.password = hashed;
+      //database action
+    }
   } catch (error) {
     res.status(500).json("Server Error");
   }
 });
 
-router.post("/login", (req, res, next) => {
+router.post("/login", async (req, res, next) => {
   try {
   } catch (error) {
     res.status(500).json("Server Error");
---
+++ api/users/users-model.js
@@ -0,0 +1,5 @@
+const db = require("../../data/dbConfig");
+
+function getAll() {
+  return db("make db for this");
+}
