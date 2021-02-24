e98e285b95c11734252015c27958458022a3b4ce
Nathan Hietala <Nrhietala@gmail.com>
2021-02-23 18:00:24 -0600

    working on auth endpoints

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,3 +1,4 @@
+const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
@@ -5,20 +6,22 @@ const User = require("../users/users-model");
 
 const { isValid } = require("../../utils/validation");
 
-const router = express().Router();
+const router = express.Router();
 
 router.post("/register", async (req, res, next) => {
   const credentials = req.body;
   try {
     if (isValid(credentials)) {
-      // add in validation
       const rounds = process.env.BCRYPT_ROUNDS || 8;
       const hashed = bcrypt.hashSync(credentials.password, rounds);
       credentials.password = hashed;
-      //database action
+      const newUser = await User.add(credentials);
+      res.status(201).json({ newUser });
+    } else {
+      res.status(400).json("Please provide valid username and password");
     }
   } catch (error) {
-    res.status(500).json("Server Error");
+    res.status(500).json({ message: error.message });
   }
 });
 
--- api/server.js
+++ api/server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const server = express();
 
 const userRouter = require("./users/users-router");
+const authRouter = require("./auth/auth-router");
 
 server.use(express.json());
 server.use("/api/users", userRouter);
+server.use("/api/auth", authRouter);
 
 server.get("/", (_, res) => [
   res.send("Is your API running? You should go catch it! 🏃‍♂️"),
